bcd0328867bd649b9981f0a47d78843e10209fc5
Gokt61 <49290553+Gokt61@users.noreply.github.com>
2021-09-21 18:28:25 +0300

    Horizontal ve Vertical Obstacle Ekledim

--- Avoid2DTest/Assets/Scripts/Spawner Scripts/ObstacleSpawner.cs
+++ Avoid2DTest/Assets/Scripts/Spawner Scripts/ObstacleSpawner.cs
@@ -5,10 +5,32 @@ using UnityEngine;
 public class ObstacleSpawner : MonoBehaviour
 {
     public Obstacle point;
+    public Obstacle horizontalObstacle;
+    public Obstacle verticalObstacle;
+
+    private bool left, up;
 
     private void Start()
     {
         StartCoroutine(SpawnAtPoint());
+
+        RandomSpawnerChance();
+
+        SpawnVertical();
+        SpawnHorizontal();
+    }
+
+    void RandomSpawnerChance()
+    {
+        if (Random.value < 0.5)
+            left = false;
+        else
+            left = true;
+
+        if (Random.value < 0.5)
+            up = false;
+        else
+            up = true;
     }
 
     public IEnumerator SpawnAtPoint()
@@ -16,5 +38,25 @@ public class ObstacleSpawner : MonoBehaviour
         yield return new WaitForSeconds(1);
         Vector2 pointPos = new Vector2(Random.Range(-point.xBound, point.xBound), Random.Range(-point.yBound, point.yBound));
         Instantiate(point.obstacle, pointPos, Quaternion.identity);
+    }
+
+    public void SpawnVertical()
+    {
+        if (left)
+            Instantiate(verticalObstacle.obstacle, new Vector2(-verticalObstacle.xBound,
+                Random.Range(-verticalObstacle.yBound, verticalObstacle.yBound)), Quaternion.identity);
+        else
+            Instantiate(verticalObstacle.obstacle, new Vector2(verticalObstacle.xBound,
+                Random.Range(verticalObstacle.yBound, verticalObstacle.yBound)), Quaternion.identity);
+    }
+
+    public void SpawnHorizontal()
+    {
+        if (up)
+            Instantiate(horizontalObstacle.obstacle, new Vector2(Random.Range(-horizontalObstacle.xBound, horizontalObstacle.xBound),
+                horizontalObstacle.yBound), Quaternion.Euler(0,0,90));
+        else
+            Instantiate(horizontalObstacle.obstacle, new Vector2(Random.Range(horizontalObstacle.xBound, horizontalObstacle.xBound),
+                -horizontalObstacle.yBound), Quaternion.Euler(0, 0, 90));
     }
 }
